e91b3e77132951c0946f62371b4936bb61ae9182
AndriyMelnykIt <andriy523625@gmail.com>
2022-01-17 20:11:10 +0200

    userEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event/dist';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   const {asFragment} = render(<App />);
-//   expect( asFragment (<App />)).toMatchSnapshot();
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-// Різні типи вибора елемента
-
 describe("App", () => {
-	it ('render App component', () => {
+	test('render App component', async () => {
 		render(<App />);
-		screen.debug();        //регулярний вираз - перевіряється часткове співпадіння ( i - означає не чутливе до регістру)
-		expect(screen.getByText(/Search:/i)).toBeInTheDocument();
-		expect(screen.getByRole('textbox')).toBeInTheDocument();
-		expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
-		expect(screen.getByPlaceholderText('search text...')).toBeInTheDocument();
-		expect(screen.getByAltText('search image')).toBeInTheDocument();
+		await screen.findByText(/Logged in as/i)
+		expect(screen.queryByText(/Searches for React/)).toBeNull();
+		screen.debug();
+		// fireEvent.change(screen.getByRole('textbox'), {
+		// 	target: { value: "React" },
+		// });
+		userEvent.type(screen.getByRole("textbox"), "React")
+		expect(screen.queryByText(/Searches for React/)).toBeInTheDocument();
+	})
+});
+
+describe("events", () => {
+	it("checkbox click", () => {
+		const handleChange = jest.fn();
+		const { container } = render(<input type="checkbox" onChange={handleChange} />)
+		const checkbox = container.firstChild;
+		// fireEvent.click(checkbox);
+		userEvent.click(checkbox);
+		userEvent.click(checkbox, {ctrlKey: true, shiftKey: true});
+		expect(checkbox).toBeChecked();
+	});
+
+	it("double click", () => {
+		const onChange = jest.fn();
+		const { container } = render(<input type="checkbox" onChange={handleChange} />)
+		const checkbox = container.firstChild;
+		expect(checkbox).not.toBeChecked();
+		userEvent.dbclick(checkbox);
+		expect(onChange).toHaveBeenCalledTimes(2);
 	});
 });
